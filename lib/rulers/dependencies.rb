44275a46de3f270bcc8d9a2cb47640039d63bf9d
abonec <abonec@gmail.com>
2012-11-24 20:46:45 +0400

    @= prevent stacklevel too deep when const loading from file that not have const

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -1,6 +1,14 @@
 class Object
   def self.const_missing(c)
-    require Rulers.to_underscore c
-    Object.const_get c
+    # prevent stacklevel too deep when in required
+    # file not exists needed file
+    return nil if @calling_const_missing
+    @calling_const_missing = true
+    filename = Rulers.to_underscore c
+    require filename
+    klass = Object.const_get c
+    @calling_const_missing = false
+
+    klass
   end
 end
